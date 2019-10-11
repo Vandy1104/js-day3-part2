4312d6bd9540ea2e4ccdf084b284a5a1e64b78cd
Vandy1104 <53416476+Vandy1104@users.noreply.github.com>
2019-10-11 15:19:29 +1300

    Add files via upload
    For loop, do-while, while statements

---
+++ js-Loop/js/script.js
@@ -0,0 +1,40 @@
+console.log('Learning loops');
+
+//for loop
+
+// for (initial value; final value; increment/decrement) {
+//
+// }
+
+
+// for (var i=0; i<15; i++) {
+//   for (var j=0; j<i; j++){
+//   document.getElementById('result').innerHTML += '&copy';
+// }
+// document.getElementById('result').innerHTML += '<br>';
+// }
+
+
+// var name = 'Yoobee';
+// var names = ['David','Mary','Luke','John','Hulk']
+// //for (var i=0; i<5; i++) {
+// for (var j=0; j<5; j++){
+// //document.getElementById('result').innerHTML += j;
+// document.getElementById('result').innerHTML += names[j];
+// document.getElementById('result').innerHTML += '<br>';
+// }
+
+
+//While loop
+// var i=0;
+// while (i<=10){
+//   document.getElementById('result').innerHTML += i ;
+//   i++;
+// }
+
+
+var i=0;
+do{
+  document.getElementById('result').innerHTML += i ;
+  i++
+} while(i<=10);
